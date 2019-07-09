70a9deb64c5aae783e84fc9b57722f92de594d5a
Phoax <raph@RESIDENT.localdomain>
2019-07-09 14:34:49 +0200

    update readme commands

---
+++ README.md
@@ -0,0 +1,7 @@
+## Clone projects
+Execute
+`./clone-projects.sh`
+
+## Start meshui
+Execute
+`docker-compose up`
---
+++ clone-projects.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+git clone git@github.com:meshui/meshui-api.git
+git clone git@github.com:meshui/meshui-app.git
